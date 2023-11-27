e43a4911e19b93a03dfd1b39fe0c87e4408b9adb
savan <savannahwilliams243@gmail.com>
2023-11-27 13:06:43 -0800

    started writeToFile fx

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ const fs = require("fs");
 // import classes from .lib/shapes directory
 const { Triangle, Circle, Square } = require("./lib/shapes");
 
+// Function writes the SVG file using user answers from inquirer prompts
+function writeToFile(fileName, answers) {
+    
+}
